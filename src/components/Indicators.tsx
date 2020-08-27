9d198d23e9725b2661ab047b56bb80e821c12b58
Đặng Tiến Cường <tiencuong.dang@gmail.com>
2020-08-27 16:06:23 +0700

    add indicator pagination

--- src/components/Indicators.tsx
+++ src/components/Indicators.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 import React from 'react';
-import { Box, Flex } from 'theme-ui';
+import { Box, Flex, Text } from 'theme-ui';
 
 const Dot = (props: { active?: boolean; size: string }) => {
   return (
@@ -27,6 +27,22 @@ const Dot = (props: { active?: boolean; size: string }) => {
   );
 };
 
+const Pagination = (props: { current: number; total: number }) => (
+  <Flex
+    sx={{
+      alignItems: 'center',
+      color: 'white',
+      transform: 'rotate(-90deg)',
+      height: 80,
+      fontSize: 14,
+    }}
+  >
+    <Text>{props.current.toString().padStart(2, '0')}</Text>
+    <Text sx={{ opacity: 0.5, mx: '8px' }}>/</Text>
+    <Text sx={{ opacity: 0.5 }}>{props.total.toString().padStart(2, '0')}</Text>
+  </Flex>
+);
+
 export function Indicators(props: {
   total: number;
   currentIdx: number;
@@ -38,82 +54,87 @@ export function Indicators(props: {
   const { height = '80vh', width = '62px', maxDisplayingDots = 6 } = props;
   const [hoveringIdx, setHoveringIdx] = React.useState<number | null>(null);
   return (
-    <Flex
-      css={{
-        position: 'relative',
-        height,
-        width,
-        justifyContent: 'center',
-        overflow: 'hidden',
-      }}
-    >
-      <Box sx={{ opacity: 0.5 }}>
-        <div
-          css={{
-            width: '2px',
-            height: '100%',
-            ml: '50%',
-            backgroundColor: 'rgba(255,255,255,0.5)',
-            boxShadow: '0px 0px 4px 2px rgba(0,0,0,0.05)',
-          }}
-        />
-        {Array.from({ length: props.total }).map((_, idx) => (
-          <Box
-            sx={{
-              position: 'absolute',
-              left: 0,
-              top: `calc(${height} / 2 + ${height} / ${
-                maxDisplayingDots + 1
-              } * ${idx - props.currentIdx})`,
-
-              transition: 'top 400ms',
+    <Flex sx={{ height, width, flexDirection: 'column' }}>
+      <Flex
+        css={{
+          flex: 1,
+          width,
+          position: 'relative',
+          justifyContent: 'center',
+          overflow: 'hidden',
+        }}
+      >
+        <Box sx={{ opacity: 0.5 }}>
+          <div
+            css={{
+              width: '2px',
+              height: '100%',
+              ml: '50%',
+              backgroundColor: 'rgba(255,255,255,0.5)',
+              boxShadow: '0px 0px 4px 2px rgba(0,0,0,0.05)',
             }}
-          >
-            <Dot
-              key={idx}
-              size={width}
-              active={idx == props.currentIdx || idx === hoveringIdx}
-            />
-          </Box>
-        ))}
-      </Box>
-      <Box>
-        {Array.from({ length: props.total }).map((_, idx) => (
-          <Flex
-            sx={{
-              alignItems: 'center',
-              justifyContent: 'center',
-              position: 'absolute',
-              left: 0,
-              top: `calc(${height} / 2 + ${height} / ${
-                maxDisplayingDots + 1
-              } * ${idx - props.currentIdx})`,
-              width,
-              height: width,
-
-              color: 'white',
-              fontSize: 16,
-              fontWeight: 'bold',
-              cursor: 'pointer',
-
-              transition: 'top 400ms',
-            }}
-            onMouseEnter={() => setHoveringIdx(idx)}
-            onMouseLeave={() => setHoveringIdx(null)}
-            onClick={() => props.onSelect?.(idx)}
-          >
+          />
+          {Array.from({ length: props.total }).map((_, idx) => (
             <Box
               sx={{
-                cursor: 'pointer',
-                transition: 'transform 400ms',
-                transform: `scale(${
-                  idx == props.currentIdx || idx === hoveringIdx ? '1,1' : '0,0'
-                })`,
+                position: 'absolute',
+                left: 0,
+                top: `calc(${height} / 2 + ${height} / ${
+                  maxDisplayingDots + 1
+                } * ${idx - props.currentIdx})`,
+
+                transition: 'top 400ms',
               }}
-            >{`${idx + 1}`}</Box>
-          </Flex>
-        ))}
-      </Box>
+            >
+              <Dot
+                key={idx}
+                size={width}
+                active={idx == props.currentIdx || idx === hoveringIdx}
+              />
+            </Box>
+          ))}
+        </Box>
+        <Box>
+          {Array.from({ length: props.total }).map((_, idx) => (
+            <Flex
+              sx={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                position: 'absolute',
+                left: 0,
+                top: `calc(${height} / 2 + ${height} / ${
+                  maxDisplayingDots + 1
+                } * ${idx - props.currentIdx})`,
+                width,
+                height: width,
+
+                color: 'white',
+                fontSize: 16,
+                fontWeight: 'bold',
+                cursor: 'pointer',
+
+                transition: 'top 400ms',
+              }}
+              onMouseEnter={() => setHoveringIdx(idx)}
+              onMouseLeave={() => setHoveringIdx(null)}
+              onClick={() => props.onSelect?.(idx)}
+            >
+              <Box
+                sx={{
+                  cursor: 'pointer',
+                  transition: 'transform 400ms',
+                  transform: `scale(${
+                    idx == props.currentIdx || idx === hoveringIdx
+                      ? '1,1'
+                      : '0,0'
+                  })`,
+                }}
+              >{`${idx + 1}`}</Box>
+            </Flex>
+          ))}
+        </Box>
+      </Flex>
+      <Pagination current={props.currentIdx + 1} total={props.total} />
     </Flex>
   );
 }
